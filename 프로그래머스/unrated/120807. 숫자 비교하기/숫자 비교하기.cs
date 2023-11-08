e41b8e0cdfde38e06157d3ecadfa359498f297bc
joseonguk1 <149387494+joseonguk1@users.noreply.github.com>
2023-11-08 10:06:03 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.13 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        if (num1 == num2) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
